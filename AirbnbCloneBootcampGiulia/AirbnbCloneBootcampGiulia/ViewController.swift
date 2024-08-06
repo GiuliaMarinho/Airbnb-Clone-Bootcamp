57f2e4708ae99f157c1b8b5c619e6969544c658c
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-08-05 22:04:35 -0300

    Adicionando Projeto Configurado para View code

---
+++ AirbnbCloneBootcampGiulia/AirbnbCloneBootcampGiulia/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AirbnbCloneBootcampGiulia
+//
+//  Created by Giulia Marinho on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
