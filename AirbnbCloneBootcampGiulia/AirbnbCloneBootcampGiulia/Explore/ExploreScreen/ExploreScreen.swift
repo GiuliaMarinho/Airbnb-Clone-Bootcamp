17796c29e86b06bc5da1c3bbb6260c3328ed4f68
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-08-08 22:45:35 -0300

    SEARCHEBAR

--- AirbnbCloneBootcampGiulia/AirbnbCloneBootcampGiulia/Explore/ExploreScreen/ExploreScreen.swift
+++ AirbnbCloneBootcampGiulia/AirbnbCloneBootcampGiulia/Explore/ExploreScreen/ExploreScreen.swift
@@ -9,26 +9,38 @@ import Foundation
 import UIKit
 
 class ExploreScreen: UIView {
-
-  init() {
-    super.init(frame: .zero)
-    backgroundColor = .red
-    addElements()
-    configConstraints()
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  func addElements() {
-
-  }
-
-  func configConstraints() {
-    NSLayoutConstraint.activate([
-
-    ])
-  }
-
+    
+    lazy var searchBarView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 30      
+        view.layer.borderWidth = 1
+        
+        return view
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        addElements()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addElements() {
+        addSubview(searchBarView)
+    }
+    
+    func configConstraints() {
+        NSLayoutConstraint.activate([
+            
+            
+        ])
+    }
+    
 }
